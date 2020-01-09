e8ff6236d61825eafa218426aba280b5c6c3792b
Unknown <atiyah@post.bgu.ac.il>
2020-01-09 02:46:57 +0200

    statistics

--- statistics.py
+++ statistics.py
@@ -7,18 +7,6 @@ upload_recipes_from_csv()
 tags_size = {k: len(v) for k, v in tags_to_recipes.items()}
 sorted_tags_size_revers = {k: v for k, v in sorted(tags_size.items(), key=lambda item: item[1], reverse=True)}
 
-for recip in all_recipes_data:
-    extractIngredientTags(all_recipes_data[recip]['ingredients'], recip)
-
-tags_list = csv_converted_tags_recipes()
-print(tags_list)
-
-save_general_csv(the_tags_url, tags_list, 'recipe_list', None)
-
-tags_size = {k: len(v) for k, v in tags_to_recipes.items()}
-sorted_tags_size_revers = {k: v for k, v in sorted(tags_size.items(), key=lambda item: item[1], reverse=True)}
-
-print(sum(tags_size.values()))
 max_ingredient_to_show = 10
 
 counter = 1
@@ -39,3 +27,7 @@ for tag in sorted_tags_size:
         break
     print(sorted_tags_size[tag], " - ", ingredients_list[tag], " .", counter)
     counter = counter + 1
+
+
+# for recip in all_recipes_data:
+#     extractIngredientTags(all_recipes_data[recip]['ingredients'], recip)
